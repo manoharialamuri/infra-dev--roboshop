303d36fa172e84d14df763dc18613d6b7a1983e8
manu <example@gmail.com>
2026-03-11 19:27:18 +0530

    Infra

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -3,3 +3,24 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.vpc.vpc_id
 }
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project}/${var.enviornment}/public_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.public_subnet_ids)
+
+}
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project}/${var.enviornment}/private_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.private_subnet_ids)
+
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project}/${var.enviornment}/database_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.database_subnet_ids)
+
+}
