219a6d52ecaf8ec460834a7496d3306cbf4bba7c
manu <example@gmail.com>
2026-03-12 14:37:08 +0530

    terraform

--- 30.bastion/main.tf
+++ 30.bastion/main.tf
@@ -4,6 +4,19 @@ resource "aws_instance" "bastion" {
   subnet_id = local.public_subnet_ids
   vpc_security_group_ids = [local.bastion_sg_id]
   iam_instance_profile = aws_iam_instance_profile.bastion.name
+  user_data = file(bastion.sh)
+
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+    #EBS Volume tags
+    tags = merge(
+    {
+        Name = "${var.project}-${var.enviornment}-bastion"
+    },
+    local.common_tags
+  )
+  }
 
 
   tags = merge(
