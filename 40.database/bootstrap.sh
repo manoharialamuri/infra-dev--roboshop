4c7f23d41426bede1545941198b5aa23ac69f2e4
manu <example@gmail.com>
2026-03-12 15:06:24 +0530

    config

--- 40.database/bootstrap.sh
+++ 40.database/bootstrap.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+component=$1
 sudo dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/manoharialamuri/tf.ansible.roles.roboshop.git
+
+cd tf.ansible.roles.roboshop
+
+ansible-playbook -e component=$component roboshop.yaml
 
