41c9a0276f30ee92c4fcdcfd19135e24875bb4b9
manu <example@gmail.com>
2026-03-12 21:07:17 +0530

    config

--- 40.database/iam.tf
+++ 40.database/iam.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role" "mysql" {
 resource "aws_iam_policy" "mysql" {
   name         = local.mysql_policy_name
   description = "A Policy for Mysql EC2 Instance"
-  policy  = templatefile(mysql-iam-policy.json, {
+  policy  = templatefile("mysql-iam-policy.json", {
             enviornment = var.enviornment
   })
 }
