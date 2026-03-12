e03885c20434323167c3db79c35408ee9d394dec
manu <example@gmail.com>
2026-03-12 19:05:42 +0530

    config

--- 40.database/main.tf
+++ 40.database/main.tf
@@ -126,6 +126,7 @@ resource "terraform_data" "bootstrap_mysql" {
 
   provisioner "remote-exec" {
     inline = [
+        
         "chmod +x /tmp/bootstrap.sh",  
         "sudo sh /tmp/bootstrap.sh mysql"
     ] 
