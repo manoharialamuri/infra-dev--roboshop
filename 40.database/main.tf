7b1751007befa55a7eba5ab9cd32834913b18ac6
manu <example@gmail.com>
2026-03-12 15:41:47 +0530

    config

--- 40.database/main.tf
+++ 40.database/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "mongodb" {
   )
 }
 
-resource "terraform_data" "mongodb" {
+resource "terraform_data" "bootstrap" {
   triggers_replace = [
     aws_instance.mongodb.id
   ]
