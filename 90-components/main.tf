48f88223105c0319045792a3586ce41739d92450
manu <example@gmail.com>
2026-03-21 15:25:55 +0530

    infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
   for_each = var.components
-  source = "git:://https://github.com/manoharialamuri/terraform-roboshop-component.git?ref=main"
+  source = "git::https://github.com/manoharialamuri/terraform-roboshop-component.git?ref=main"
   component = each.key
   rule_priority = each.value.rule_priority
 
