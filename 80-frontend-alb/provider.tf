8860efc23affae39a1f9ba6f7464eada73a1bad6
manu <example@gmail.com>
2026-03-21 15:24:53 +0530

    infra

--- 80-frontend-alb/provider.tf
+++ 80-frontend-alb/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
   bucket = "daws8ss-remote-state-s3"
-  key = "remote_state.backend-alb.tfstate"
+  key = "remote_state.frontend-alb.tfstate"
   region = "us-east-1"
   encrypt = true
   use_lockfile = true
---
+++ 90-components/variables.tf
@@ -0,0 +1,24 @@
+variable "components" {
+  default = {
+    #this is for backend components, attaching to backend alb
+    catalogue = {
+        rule_priority = 10
+    }
+    user = {
+        rule_priority = 20
+    }
+    cart = {
+        rule_priority = 30
+    }
+    shipping = {
+        rule_priority = 40
+    }
+    payment = {
+        rule_priority = 50
+    }
+    #this is attaching to frontend alb and there is only one component attaching to frontend alb
+    frontend = {
+        rule_priority = 10
+    }
+  }
+}
