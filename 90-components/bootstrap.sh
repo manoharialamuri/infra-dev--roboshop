9f0f108ea2f503347a41e018889cfb28295097cf
manu <example@gmail.com>
2026-03-22 10:07:07 +0530

    actions

--- 90-components/bootstrap.sh
+++ 90-components/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 component=$1
-enviornment=$2
+environment=$2
 app_version=$3
 sudo dnf install ansible -y
 
@@ -10,4 +10,4 @@ git clone https://github.com/manoharialamuri/tf.ansible.roles.roboshop.git
 
 cd tf.ansible.roles.roboshop
 git pull
-ansible-playbook -e component=$component -e env=$enviornment -e app_version=$app_version roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
