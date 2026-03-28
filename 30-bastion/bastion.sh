1b48bf7704c6680ddc3f8c5698d2ec492d2cf753
manu <example@gmail.com>
2026-03-28 15:45:55 +0530

    actions

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -9,3 +9,20 @@ xfs_growfs /home
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
+
+
+# creating databases
+cd /home/ec2-user
+git clone https://github.com/manoharialamuri/infra-dev--roboshop.git
+chown ec2-user:ec2-user -R infra-dev--roboshop
+cd infra-dev--roboshop/40-databases
+terraform init
+terraform apply -auto-approve
+
+# creating components
+cd /home/ec2-user
+git clone https://github.com/manoharialamuri/infra-dev--roboshop.git
+chown ec2-user:ec2-user -R infra-dev--roboshop
+cd roboshop-infra-dev/90-components
+terraform init
+terraform apply -auto-approve
